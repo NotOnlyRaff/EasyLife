d7a1d873ec14e2b7ba534be5dea0e6c1855e8ad0
Raffaele <raffaelepellecchia27@gmail.com>
2025-05-06 18:57:30 +0200

    First Commit

---
+++ src/main/java/com/easylife/easylife/EasyLifeApplication.java
@@ -0,0 +1,13 @@
+package com.easylife.easylife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easylife/easylife/EasyLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easylife.easylife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
